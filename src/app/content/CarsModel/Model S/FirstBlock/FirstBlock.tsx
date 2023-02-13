1afdd64f6f3fb1dad1c16476f89c9f6c92e2bef6
Nazaruk-D <nazaruk-dima@mail.ru>
2023-02-13 17:57:28 +0300

    feat: Characteristics Component

--- src/app/content/CarsModel/Model S/FirstBlock/FirstBlock.tsx
+++ src/app/content/CarsModel/Model S/FirstBlock/FirstBlock.tsx
@@ -3,14 +3,23 @@ import PresentationBlock from "../../../../../common/components/PresentationBloc
 // @ts-ignore
 import modelS from "../../../../../common/assets/modelS.jfif";
 import Button from "../../../../../common/components/Button/Button";
+import Characteristics from "../../../../../common/components/Characteristics/Characteristics";
 
 const FirstBlock = () => {
     return (
         <PresentationBlock backgroundImage={modelS} model={'Model S'} conditions={'Plaid'}>
-            <div style={{marginRight: 20}}>New comp A</div>
-            <div style={{marginRight: 20}}>New comp A</div>
-            <div style={{marginRight: 20}}>New comp A</div>
-            <div style={{marginRight: 20}}>New comp A</div>
+            <div style={{marginRight: 20}}>
+                <Characteristics topText={'396'}  measurement={'mi'} bottomText={'Range (EPA est.)'}/>
+            </div>
+            <div style={{marginRight: 20}}>
+                <Characteristics topText={'396'}  measurement={'mi'} bottomText={'Range (EPA est.)'}/>
+            </div>
+            <div style={{marginRight: 20}}>
+                <Characteristics topText={'396'}  measurement={'mi'} bottomText={'Range (EPA est.)'}/>
+            </div>
+            <div style={{marginRight: 20}}>
+                <Characteristics topText={'396'}  measurement={'mi'} bottomText={'Range (EPA est.)'}/>
+            </div>
             <Button title={"New button"} backgroundColor={'rgba(0, 0, 0, 0)'} color={'white'} onClick={()=>{}}/>
         </PresentationBlock>
     );
---
+++ src/common/components/Characteristics/Characteristics.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import s from './Characteristics.module.scss'
+
+type CharacteristicsPropsType = {
+    topText: string
+    measurement?: string
+    bottomText: string
+}
+
+const Characteristics: React.FC<CharacteristicsPropsType> = ({bottomText, topText, measurement}) => {
+    return (
+        <div className={s.characteristicsBlock}>
+            <div className={s.topTextBlock}>
+                <div className={s.mainInfo}>{topText}</div>
+                <div className={s.measurement}>{measurement}</div>
+            </div>
+            <div className={s.bottomText}>
+                <p>{bottomText}</p>
+            </div>
+        </div>
+    );
+};
+
+export default Characteristics;
