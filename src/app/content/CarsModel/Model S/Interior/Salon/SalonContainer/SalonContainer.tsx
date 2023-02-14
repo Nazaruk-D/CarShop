0113eb0aaeb8395bd5b4ef35c43e4249e8fa2f51
Nazaruk-D <nazaruk-dima@mail.ru>
2023-02-14 20:05:50 +0300

    feat: Salon component

--- src/app/content/CarsModel/Model S/Interior/Salon/SalonContainer/SalonContainer.tsx
+++ src/app/content/CarsModel/Model S/Interior/Salon/SalonContainer/SalonContainer.tsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import s from './SalonContainer.module.scss'
 
-const SalonContainer = () => {
-    return (
-        <div className={s.salonContainer}>
+type SalonContainerPropType = {
+    backgroundImage?: string
+    videoLink?: string
+    title: string
+    description: string
+    position?: "row-reverse"
+    start?: "flex-start"
+}
 
+const SalonContainer: React.FC<SalonContainerPropType> = ({videoLink, position, description, title, backgroundImage, start}) => {
+    return (
+        <div className={s.salonContainer} style={{flexDirection: position}}>
+            <div className={s.image} style={{backgroundImage: `url(${backgroundImage})`}}>
+                        <video
+                            autoPlay
+                            id="my-player"
+                            className={s.videoJs}
+                            preload="auto"
+                            muted
+                            playsInline
+                            controls={false}
+                            data-autoplay-desktop={true}
+                            data-autoplay-portrait={true}
+                            data-autoplay-mobile={true}
+                            data-play-on-hover={false}
+                            src={videoLink}
+                            loop
+                            data-setup='{}'>
+                        </video>
+            </div>
+            <div className={s.descriptionContainer} style={{justifyContent: start}}>
+                <div className={s.descriptionBlock}>
+                    <div className={s.title}>{title}</div>
+                    <div className={s.description}>{description}</div>
+                </div>
+            </div>
         </div>
     );
 };
