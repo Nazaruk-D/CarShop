e6f62fc1828625398f7f0fa6d650e30d42705947
naughtymor <znaknikita.it@gmail.com>
2023-02-15 13:12:29 +0300

    refactor: use common Advantages Component

--- src/app/content/CarsModel/Model Y/ModelY.tsx
+++ src/app/content/CarsModel/Model Y/ModelY.tsx
@@ -3,19 +3,33 @@ import Header from "../../../header/Header";
 import FirstBlock from "./FirstBlock/FirstBlock";
 import Safety from "./Safety/Safety";
 import Comfort from "./Comfort/Comfort";
-import Advantages from "./Advantages/Advantages";
 import Autopilot from "./Autopilot/Autopilot";
 import Interior from "./Interior/Interior";
+import {CharacteristicsPropsType} from "../../../../common/components/Characteristics/Characteristics";
+//@ts-ignore
+import modelYRed from '../../../../common/assets/modelYRed.jfif'
+import Advantages from "../commonComponents/Advantages/Advantages";
+
+const advantagesData: CharacteristicsPropsType[] = [
+    {
+        topText: '2', measurement: '', bottomText: 'Independent motors digitally control torque to the front and rear wheels',
+    },
+    {
+        topText: '3.5', measurement: 's', bottomText: 'Quickest acceleration—from zero to 60 mph* in as little as 3.5 seconds',
+    },
+    {
+        topText: 'AWD', measurement: '', bottomText: 'Capable in rain, snow, mud and off-road with superior traction control',
+    },
+]
 
 const ModelY = () => {
-    //переделать advantages на common компонент
     return (
         <div>
             <Header/>
             <FirstBlock/>
             <Safety/>
             <Comfort/>
-            <Advantages/>
+            <Advantages data={advantagesData} backgroundImage={modelYRed} title={'Dual Motor'} subtitle={'All-Wheel Drive'} description={'Tesla All-Wheel Drive has two ultra-responsive, independent electric motors that digitally control torque to the front and rear wheels—for far better handling, traction and stability control. Model Y is capable in rain, snow, mud and off-road.'}/>
             <Autopilot/>
             <Interior/>
         </div>
