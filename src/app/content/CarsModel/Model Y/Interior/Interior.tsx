292ba036885c7181cde4c3f2be7dcf836987d653
naughtymor <znaknikita.it@gmail.com>
2023-02-15 23:26:04 +0300

    refactor: Interior Component

--- src/app/content/CarsModel/Model Y/Interior/Interior.tsx
+++ src/app/content/CarsModel/Model Y/Interior/Interior.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import s from './Interior.module.scss'
-//@ts-ignore
-import modelYInterior from '../../../../../common/assets/modelYInterior.jfif'
-import wifiLogo from '../../../../../common/assets/wifiLogo.svg'
-import teslaTopViewIcon from '../../../../../common/assets/teslaTopViewIcon.svg'
+import s from './Interior.module.scss';
 import Technologies from "../../../../../common/components/Technologies/Technologies";
 import TitleBlock from "../../../../../common/components/TitleBlock/TitleBlock";
+import wifiLogo from '../../../../../common/assets/wifiLogo.svg';
+import teslaTopViewIcon from '../../../../../common/assets/teslaTopViewIcon.svg';
+//@ts-ignore
+import modelYInterior from '../../../../../common/assets/modelYInterior.jfif';
 
 const Interior = () => {
     return (
@@ -20,7 +20,9 @@ const Interior = () => {
                                   image={teslaTopViewIcon}/>
                 </div>
             </div>
-            <TitleBlock title={'Built Around the Driver'} subtitle={'Interior'} description={'With an elevated seating position and low dash, the driver has a commanding view of the road ahead. The interior of Model Y is simple and clean, with a 15-inch touch screen, immersive sound system and an expansive all-glass roof that creates extra headroom and provides a seamless view of the sky.'}/>
+            <TitleBlock title={'Built Around the Driver'}
+                        subtitle={'Interior'}
+                        description={'With an elevated seating position and low dash, the driver has a commanding view of the road ahead. The interior of Model Y is simple and clean, with a 15-inch touch screen, immersive sound system and an expansive all-glass roof that creates extra headroom and provides a seamless view of the sky.'}/>
         </div>
     );
 };
