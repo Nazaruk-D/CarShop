9cb55d153542ddc801a49046a2f457a36cdfcd6c
naughtymor <znaknikita.it@gmail.com>
2023-02-15 14:06:01 +0300

    refactor: image and text changing in Comfort Component

--- src/app/content/CarsModel/Model Y/Comfort/Comfort.tsx
+++ src/app/content/CarsModel/Model Y/Comfort/Comfort.tsx
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './Comfort.module.scss'
-import TitleBlock from "../../../../../common/components/TitleBlock/TitleBlock";
 //@ts-ignore
 import firstInteriorModelY from '../../../../../common/assets/firstInteriorModelY.jfif'
+//@ts-ignore
+import secondInteriorModelY from '../../../../../common/assets/secondInteriorModelY.jfif'
+import RoundButton from "../../../../../common/components/RoundButton/RoundButton";
+
 
 const Comfort = () => {
+    const [isFirstActive, setIsFirstActive] = useState(true)
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setIsFirstActive(!isFirstActive)
+        }, 6000)
+        return () => clearTimeout(timer);
+    }, [isFirstActive])
+
     return (
         <div className={s.comfortContainer}>
            <div className={s.imageBlock}>
-                <div className={s.imageContainer} style={{backgroundImage: `url(${firstInteriorModelY})`}}></div>
+                <div className={s.imageContainer} style={{backgroundImage: `url(${isFirstActive ? firstInteriorModelY : secondInteriorModelY})`}}></div>
             </div>
             <div className={s.descriptionBlock}>
                 <div className={s.descriptionContainer}>
                     <div className={s.buttonsBlock}>
-                        <div className={s.buttonOne} onClick={()=>{}}></div>
-                        <div className={s.buttonTwo} onClick={()=>{}}></div>
+                        <RoundButton color={isFirstActive ? 'black' : '#c9c9c9'} onClick={() => {setIsFirstActive(true)}}/>
+                        <RoundButton color={isFirstActive ? '#c9c9c9' : 'black'} onClick={() => {setIsFirstActive(false)}}/>
                     </div>
                     <div className={s.description}>
-                        Versatile seating and storage for cargo and passengers
+                        {isFirstActive
+                        ? 'Versatile seating and storage for cargo and passengers'
+                        : 'Room for up to seven with optional third row'}
                     </div>
                 </div>
             </div>
