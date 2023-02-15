26e5b5b723a950b89e7b6284bb1782a10bbed946
naughtymor <znaknikita.it@gmail.com>
2023-02-15 21:54:55 +0300

    feat: Order Component

--- src/app/content/CarsModel/Model Y/ModelY.tsx
+++ src/app/content/CarsModel/Model Y/ModelY.tsx
@@ -15,6 +15,9 @@ import modelYRed from '../../../../common/assets/modelYRed.jfif'
 import modelYRangeVideo from "../../../../common/assets/video/modelYRangeVideo.mp4";
 // @ts-ignore
 import modelYBlack from '../../../../common/assets/modelYBlack.jfif'
+// @ts-ignore
+import sideModelY from '../../../../common/assets/sideModelY.jfif'
+import Order from "../commonComponents/Order/Order";
 
 const advantagesData: CharacteristicsPropsType[] = [
     {
@@ -143,7 +146,11 @@ const ModelY = () => {
                     description={'Model Y is fully electric, so you never need to visit a gas station again. If you charge overnight at home, you can wake up to a full battery every morning. And when you’re on the road, it’s easy to plug in along the way—at any public station or with the Tesla charging network. We currently have over 40,000 Superchargers worldwide, with six new locations opening every week. Chat with a Tesla Advisor to learn more about Model Y or schedule a demo drive today.'}/>
             <Autopilot/>
             <Interior/>
-            <Specs title={'Model Y'} firstSpecsData={performanceSpecsData} secondSpecsData={longRangeSpecsData} backgroundImage={modelYBlack}/>
+            <Specs title={'Model Y'}
+                   firstSpecsData={performanceSpecsData}
+                   secondSpecsData={longRangeSpecsData}
+                   backgroundImage={modelYBlack}/>
+            <Order carName={'Model Y'} backgroundImage={sideModelY}/>
         </div>
     );
 };
