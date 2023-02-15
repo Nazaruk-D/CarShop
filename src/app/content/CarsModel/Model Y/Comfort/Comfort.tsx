89679d5a9dcdd7cb43ebb24161fc50e84540d37c
naughtymor <znaknikita.it@gmail.com>
2023-02-15 18:19:30 +0300

    refactor: Safety Component

--- src/app/content/CarsModel/Model Y/Comfort/Comfort.tsx
+++ src/app/content/CarsModel/Model Y/Comfort/Comfort.tsx
@@ -5,6 +5,7 @@ import firstInteriorModelY from '../../../../../common/assets/firstInteriorModel
 //@ts-ignore
 import secondInteriorModelY from '../../../../../common/assets/secondInteriorModelY.jfif'
 import RoundButton from "../../../../../common/components/RoundButton/RoundButton";
+import TitleBlock from "../../../../../common/components/TitleBlock/TitleBlock";
 
 
 const Comfort = () => {
@@ -18,20 +19,29 @@ const Comfort = () => {
     }, [isFirstActive])
 
     return (
-        <div className={s.comfortContainer}>
-           <div className={s.imageBlock}>
-                <div className={s.imageContainer} style={{backgroundImage: `url(${isFirstActive ? firstInteriorModelY : secondInteriorModelY})`}}></div>
-            </div>
-            <div className={s.descriptionBlock}>
-                <div className={s.descriptionContainer}>
-                    <div className={s.buttonsBlock}>
-                        <RoundButton color={isFirstActive ? 'black' : '#c9c9c9'} onClick={() => {setIsFirstActive(true)}}/>
-                        <RoundButton color={isFirstActive ? '#c9c9c9' : 'black'} onClick={() => {setIsFirstActive(false)}}/>
-                    </div>
-                    <div className={s.description}>
-                        {isFirstActive
-                        ? 'Versatile seating and storage for cargo and passengers'
-                        : 'Room for up to seven with optional third row'}
+        <div className={s.mainContainer}>
+            <TitleBlock title={'A Place For Everything'} subtitle={'Utility'}
+                        description={'Model Y provides maximum versatility—able to carry 7 passengers and their cargo. Each second row seat folds flat independently, creating flexible storage for skis, furniture, luggage and more. The liftgate opens to a low trunk floor that makes loading and unloading easy and quick. Chat with a Tesla Advisor to learn more about Model Y or schedule a demo drive today.'}/>
+            <div className={s.comfortContainer}>
+                <div className={s.imageBlock}>
+                    <div className={s.imageContainer}
+                         style={{backgroundImage: `url(${isFirstActive ? firstInteriorModelY : secondInteriorModelY})`}}></div>
+                </div>
+                <div className={s.descriptionBlock}>
+                    <div className={s.descriptionContainer}>
+                        <div className={s.buttonsBlock}>
+                            <RoundButton color={isFirstActive ? 'black' : '#c9c9c9'} onClick={() => {
+                                setIsFirstActive(true)
+                            }}/>
+                            <RoundButton color={isFirstActive ? '#c9c9c9' : 'black'} onClick={() => {
+                                setIsFirstActive(false)
+                            }}/>
+                        </div>
+                        <div className={s.description}>
+                            {isFirstActive
+                                ? 'Versatile seating and storage for cargo and passengers'
+                                : 'Room for up to seven with optional third row'}
+                        </div>
                     </div>
                 </div>
             </div>
--- src/app/content/CarsModel/Model Y/ModelY.tsx
+++ src/app/content/CarsModel/Model Y/ModelY.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import Header from "../../../header/Header";
 import FirstBlock from "./FirstBlock/FirstBlock";
-import Safety from "./Safety/Safety";
 import Comfort from "./Comfort/Comfort";
 import Autopilot from "./Autopilot/Autopilot";
 import Interior from "./Interior/Interior";
+import safetyImage from '../../../../common/assets/safetyImage.png'
 import {CharacteristicsPropsType} from "../../../../common/components/Characteristics/Characteristics";
 //@ts-ignore
 import modelYRed from '../../../../common/assets/modelYRed.jfif'
 import Advantages from "../commonComponents/Advantages/Advantages";
 import Specs from "./Specs/Specs";
+import Safety from "../commonComponents/Safety/Safety";
 
 const advantagesData: CharacteristicsPropsType[] = [
     {
-        topText: '2', measurement: '', bottomText: 'Independent motors digitally control torque to the front and rear wheels',
+        topText: '2',
+        measurement: '',
+        bottomText: 'Independent motors digitally control torque to the front and rear wheels',
     },
     {
-        topText: '3.5', measurement: 's', bottomText: 'Quickest acceleration—from zero to 60 mph* in as little as 3.5 seconds',
+        topText: '3.5',
+        measurement: 's',
+        bottomText: 'Quickest acceleration—from zero to 60 mph* in as little as 3.5 seconds',
     },
     {
-        topText: 'AWD', measurement: '', bottomText: 'Capable in rain, snow, mud and off-road with superior traction control',
+        topText: 'AWD',
+        measurement: '',
+        bottomText: 'Capable in rain, snow, mud and off-road with superior traction control',
     },
 ]
 
@@ -28,9 +35,12 @@ const ModelY = () => {
         <div>
             <Header/>
             <FirstBlock/>
-            <Safety/>
+            <Safety backgroundImage={safetyImage} subtitle={'Safety'} title={'Designed for Safety'} flexDirection={'row-reverse'}
+                    description={'Safety is the most important part of every Tesla. We design our vehicles to exceed safety standards. 5-Star Rating Model Y achieved NHTSA 5-star safety ratings in every category and subcategory. Top Safety Pick Model Y received the IIHS Top Safety Pick+ award, with top ratings in all crashworthiness and front crash prevention categories.'}/>
             <Comfort/>
-            <Advantages data={advantagesData} backgroundImage={modelYRed} title={'Dual Motor'} subtitle={'All-Wheel Drive'} description={'Tesla All-Wheel Drive has two ultra-responsive, independent electric motors that digitally control torque to the front and rear wheels—for far better handling, traction and stability control. Model Y is capable in rain, snow, mud and off-road.'}/>
+            <Advantages data={advantagesData} backgroundImage={modelYRed} title={'Dual Motor'}
+                        subtitle={'All-Wheel Drive'}
+                        description={'Tesla All-Wheel Drive has two ultra-responsive, independent electric motors that digitally control torque to the front and rear wheels—for far better handling, traction and stability control. Model Y is capable in rain, snow, mud and off-road.'}/>
             <Autopilot/>
             <Interior/>
             <Specs/>
