295518e95f3cb4c4b72f41fe6931d102226ab53c
naughtymor <znaknikita.it@gmail.com>
2023-02-16 21:40:40 +0300

    feat: Safety Component

--- src/app/content/CarsModel/Model 3/Model3.tsx
+++ src/app/content/CarsModel/Model 3/Model3.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import TopBlock from "./TopBlock/TopBlock";
+import Safety from "../commonComponents/Safety/Safety";
+//@ts-ignore
+import model3Safety from "../../../../common/assets/model3Safety.jfif";
 
 const Model3 = () => {
     return (
         <div>
             <TopBlock/>
+            <Safety backgroundImage={model3Safety}
+                    backgroundColor={'white'}
+                    subtitle={'Safety'}
+                    title={'Built for Safety'}
+                    description={'Safety is the most important part of every Tesla. We design our vehicles to exceed safety standards. 5-Star Rating Model Y achieved NHTSA 5-star safety ratings in every category and subcategory. Top Safety Pick Model Y received the IIHS Top Safety Pick+ award, with top ratings in all crashworthiness and front crash prevention categories.'}/>
         </div>
     );
 };
