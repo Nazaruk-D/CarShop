07b90ed53926f4914f61aeb19eb1f51fae6477b0
naughtymor <znaknikita.it@gmail.com>
2023-02-18 11:44:42 +0300

    refactor: containerization in Features Component

--- src/app/content/CarsModel/commonComponents/Features/Features.tsx
+++ src/app/content/CarsModel/commonComponents/Features/Features.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import s from './Features.module.scss'
 import VideoPlayer from "../../../../../common/components/VideoPlayer/VideoPlayer";
+import TitleBlock from "../../../../../common/components/TitleBlock/TitleBlock";
 
 export type FeaturesPropsType = {
     data: DataPropsType
@@ -9,6 +10,7 @@ export type FeaturesPropsType = {
 export type DataPropsType = {
     title: string
     description: string
+    subtitle?: string
     infoBlock: FeaturesPropsData[]
 }
 
@@ -23,6 +25,8 @@ type FeaturesPropsData = {
 
 const Features: React.FC<FeaturesPropsType> = ({data}) => {
     const [item, setItem] = useState(0)
+    const style = data.subtitle ? {width: "95%"} : {}
+    const styleWide = data.subtitle ? {} : {height: "135vh"}
 
     useEffect(() => {
         const timer = setInterval(() => {
@@ -36,28 +40,54 @@ const Features: React.FC<FeaturesPropsType> = ({data}) => {
     }, [item])
 
     return (
-        <div className={s.travelMapContainer}>
-            <div className={s.travelMapBlock}>
-                <div className={s.text}>
-                    <h2 className={s.title}>
-                        {data.title}
-                    </h2>
-                    <div className={s.description}>
-                        {data.description}
-                    </div>
-                </div>
-                <div className={s.map} style={{backgroundImage: `url(${data.infoBlock[item].image})`}}>
-                    <VideoPlayer link={data.infoBlock[item].image} style={{"width": "100%"}}/>
-                </div>
-                <div className={s.infoBlock}>
-                    {data.infoBlock.map((d, i) => <div className={s.info}
-                                                   key={i}
-                                                   style={{opacity: item === i ? 1 : 0.5}}
-                                                   onClick={() => setItem(i)}>
-                        <div className={s.title}>{d.title}</div>
-                        <div className={s.distance}>{d.distance}</div>
-                        <div>{d.description}</div>
-                    </div>)}
+        <div className={s.mainContainer}>
+            {data.subtitle && <TitleBlock title={data.title}
+                                          subtitle={data.subtitle}
+                                          description={data.description}/>}
+            <div className={s.travelMapContainer} style={styleWide}>
+                <div className={s.travelMapBlock} style={style}>
+                    {data.subtitle
+                    ? <>
+                            <div className={s.map}
+                                 style={{backgroundImage: `url(${data.infoBlock[item].image})`, height: '75%'}}>
+                                <VideoPlayer link={data.infoBlock[item].image} style={{"width": "100%", "height": "100%", "object-fit": "cover", "border-radius": '30px'}}/>
+                            </div>
+                            <div className={s.infoBlock}>
+                                {data.infoBlock.map((d, i) => <div className={s.wideInfo}
+                                                                   key={i}
+                                                                   style={{opacity: item === i ? 1 : 0.5, borderTop: item === i ? '3px solid black' : '2px solid black'}}
+                                                                   onClick={() => setItem(i)}>
+                                    <div className={s.title}>{d.title}</div>
+                                    <div className={s.distance}>{d.distance}</div>
+                                    <div>{d.description}</div>
+                                </div>)}
+                            </div>
+                        </>
+                    : <>
+                            <div className={s.text}>
+                                <h2 className={s.title}>
+                                    {data.title}
+                                </h2>
+                                <div className={s.description}>
+                                    {data.description}
+                                </div>
+                            </div>
+                            <div className={s.map}
+                                 style={{backgroundImage: `url(${data.infoBlock[item].image})`}}>
+                                <VideoPlayer link={data.infoBlock[item].image} style={{"width": "100%", "object-fit": "cover", "height": "100%"}}/>
+                            </div>
+                            <div className={s.infoBlock}>
+                                {data.infoBlock.map((d, i) => <div className={s.info}
+                                                                   key={i}
+                                                                   style={{opacity: item === i ? 1 : 0.5, borderTop: item === i ? '4px solid black' : '2px solid black'}}
+                                                                   onClick={() => setItem(i)}>
+                                    <div className={s.title}>{d.title}</div>
+                                    <div className={s.distance}>{d.distance}</div>
+                                    <div>{d.description}</div>
+                                </div>)}
+                            </div>
+                        </>
+                    }
                 </div>
             </div>
         </div>
