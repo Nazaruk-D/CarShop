e9707f95bda14ae75fbf6cdd0a86133f2a6a0308
naughtymor <znaknikita.it@gmail.com>
2023-02-24 17:22:37 +0300

    feat: Form to ContactBlock

--- src/app/content/Schedule/ScheduleCar/ContactBlock/ContactBlock.tsx
+++ src/app/content/Schedule/ScheduleCar/ContactBlock/ContactBlock.tsx
@@ -20,7 +20,7 @@ const ContactBlock = () => {
                     <h1 className={s.title}>Contact Information</h1>
                 </div>
                 <div className={s.formBlock}>
-                    <Formik initialValues={{ email: '', firstName: '', lastName: '', phoneNumber: '', contactPreference: '', zipCode: '', region: '' }}
+                    <Formik initialValues={{ email: '', firstName: '', lastName: '', phoneNumber: '+375', contactPreference: '', zipCode: '', region: '' }}
                             validate={values => {
                             const errors: ErrorsType = {};
                                 if (!values.email) {
@@ -65,66 +65,102 @@ const ContactBlock = () => {
                               handleSubmit,
                           }) => (
                             <Form onSubmit={handleSubmit} className={s.form}>
-                                <div className={s.inputBlock}>
-                                    <span className={s.label}>First Name</span>
-                                    <Field
-                                        type="text"
-                                        name="firstName"
-                                        onChange={handleChange}
-                                        value={values.firstName}
-                                        style={errors.firstName ? {border: `1px solid #bd1010`} : null}
-                                    />
-                                    {errors.firstName && touched.firstName && <span className={s.error}>{errors.firstName}</span>}
+                                <div className={s.inputsContainer}>
+                                    <div className={s.inputBlock}>
+                                        <span className={s.label}>First Name</span>
+                                        <Field
+                                            type="text"
+                                            name="firstName"
+                                            onChange={handleChange}
+                                            value={values.firstName}
+                                            // style={errors.firstName ? {border: `1px solid #bd1010`} : null}
+                                        />
+                                        {errors.firstName && touched.firstName && <span className={s.error}>{errors.firstName}</span>}
+                                    </div>
+                                    <div className={s.inputBlock}>
+                                        <span className={s.label}>Last Name</span>
+                                        <Field
+                                            type="text"
+                                            name="lastName"
+                                            onChange={handleChange}
+                                            value={values.lastName}
+                                            // style={errors.lastName ? {border: `1px solid #bd1010`} : null}
+                                        />
+                                        {errors.lastName && touched.lastName && <span className={s.error}>{errors.lastName}</span>}
+                                    </div>
+                                    <div className={s.inputBlock}>
+                                        <span className={s.label}>Contact Preference</span>
+                                        <Field
+                                            component="select"
+                                            name="contactPreference"
+                                            onChange={handleChange}
+                                            value={values.contactPreference}
+                                        >
+                                            <option value="PN">Phone Number</option>
+                                            <option value="EA">Email Address</option>
+                                        </Field>
+                                    </div>
+                                    <div className={s.inputBlock}>
+                                        <span className={s.label}>Phone Number</span>
+                                        <Field
+                                            type="text"
+                                            name="phoneNumber"
+                                            onChange={handleChange}
+                                            value={values.phoneNumber}
+                                            // style={errors.phoneNumber ? {border: `1px solid #bd1010`} : null}
+                                        />
+                                        {errors.phoneNumber && touched.phoneNumber && <span className={s.error}>{errors.phoneNumber}</span>}
+                                    </div>
+                                    <div className={s.inputBlock}>
+                                        <span className={s.label}>Email</span>
+                                        <Field
+                                            type="email"
+                                            name="email"
+                                            onChange={handleChange}
+                                            value={values.email}
+                                            // style={errors.email ? {border: `1px solid #bd1010`} : null}
+                                        />
+                                        {errors.email && touched.email && <span className={s.error}>{errors.email}</span>}
+                                    </div>
+                                    <div className={s.inputBlock}>
+                                        <span className={s.label}>Region</span>
+                                        <Field
+                                            component="select"
+                                            name="region"
+                                            onChange={handleChange}
+                                            value={values.region}
+                                        >
+                                            <option value="BLR">Belarus</option>
+                                            <option value="US">United States</option>
+                                        </Field>
+                                    </div>
+                                    <div className={s.inputBlock}>
+                                        <span className={s.label}>Zip Code</span>
+                                        <Field
+                                            type="text"
+                                            name="zipCode"
+                                            onChange={handleChange}
+                                            value={values.zipCode}
+                                            // style={errors.zipCode ? {border: `1px solid #bd1010`} : null}
+                                        />
+                                        {errors.zipCode && touched.zipCode && <span className={s.error}>{errors.zipCode}</span>}
+                                    </div>
                                 </div>
-
-                                <Field
-                                    type="text"
-                                    name="lastName"
-                                    onChange={handleChange}
-                                    value={values.lastName}
-                                />
-                                {errors.lastName && touched.lastName && errors.lastName}
-                                <Field
-                                    component="select"
-                                    name="contactPreference"
-                                    onChange={handleChange}
-                                    value={values.contactPreference}
-                                >
-                                    <option value="EA">Email Address</option>
-                                    <option value="PN">Phone Number</option>
-                                </Field>
-                                <Field
-                                    type="text"
-                                    name="phoneNumber"
-                                    onChange={handleChange}
-                                    value={values.phoneNumber}
-                                />
-                                {errors.phoneNumber && touched.phoneNumber && errors.phoneNumber}
-                                <Field
-                                    type="email"
-                                    name="email"
-                                    onChange={handleChange}
-                                    value={values.email}
-                                />
-                                {errors.email && touched.email && errors.email}
-                                <Field
-                                    component="select"
-                                    name="region"
-                                    onChange={handleChange}
-                                    value={values.region}
-                                >
-                                    <option value="US">United States</option>
-                                    <option value="BLR">Belarus</option>
-                                </Field>
-                                <Field
-                                    type="text"
-                                    name="zipCode"
-                                    onChange={handleChange}
-                                    value={values.zipCode}
-                                />
-                                {errors.zipCode && touched.zipCode && errors.zipCode}
-                                <button type="submit">
-                                    Submit
+                                <div className={s.checkboxesContainer}>
+                                    <div className={s.checkboxBlock}>
+                                        <Field type="checkbox" name="interested" />
+                                        <span className={s.title}>I’m interested in solar and Powerwall</span>
+                                    </div>
+                                    <div className={s.checkboxBlock}>
+                                        <Field type="checkbox" name="updates" />
+                                        <span className={s.title}>Get Tesla Updates</span>
+                                    </div>
+                                </div>
+                                <div className={s.descriptionContainer}>
+                                    <span className={s.description}>By clicking "Submit & Continue" I agree to share the provided information with Tesla to be contacted with more details or offers about Tesla products. I understand these calls or texts may use computer-assisted dialing or pre-recorded messages. This consent is not a condition of the demo drive.</span>
+                                </div>
+                                <button type="submit" className={s.submitButton}>
+                                    Submit and Continue
                                 </button>
                             </Form>
                         )}
@@ -136,36 +172,3 @@ const ContactBlock = () => {
 }
 
 export default ContactBlock;
-
-
-// <Formik
-//     initialValues={initialValues}
-//     onSubmit={(values, actions) => {
-//         console.log({ values, actions });
-//     }}>
-//     <Form>
-//         <Field id="firstName"
-//                name="firstName"
-//                placeholder="First Name" />
-//         <Field id="lastName"
-//                name="lastName"
-//                placeholder="Last Name" />
-//         <Field id="contactPreference"
-//                name="contactPreference"
-//                placeholder="Contact Preference" />
-//         <Field id="phoneNumber"
-//                name="phoneNumber"
-//                placeholder="Phone Number" />
-//         <Field id="emailAddress"
-//                name="emailAddress"
-//                placeholder="Email Address"
-//                type="email"/>
-//         <Field id="region"
-//                name="region"
-//                placeholder="Region" />
-//         <Field id="zipCode"
-//                name="zipCode"
-//                placeholder="Zip Code" />
-//         <button type="submit">Submit</button>
-//     </Form>
-// </Formik>
