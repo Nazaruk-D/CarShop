2cd39aced3f6a6f6cb14b53a1ebcfa18e7b6d40e
Nazaruk-D <nazaruk-dima@mail.ru>
2023-02-26 15:09:24 +0300

    fix: baseURL in instance

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-    baseURL: 'https://test.herokuapp.com/2.0/',
+    // baseURL: 'https://backend-tesla-m8e8sus9r-nazaruk-d.vercel.app/',
+    baseURL: 'http://localhost:7542/',
     withCredentials: true,
 })
