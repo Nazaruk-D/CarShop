e30b0b37f86189ffeb28c8158e553168168d3577
naughtymor <znaknikita.it@gmail.com>
2023-02-28 20:58:06 +0300

    feat: ProfileSettings Component

--- src/features/profile/MainBlock/Orders/Orders.tsx
+++ src/features/profile/MainBlock/Orders/Orders.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import s from "./Orders.module.scss"
 
 const Orders = () => {
-
     return (
         <div className={s.mainBlock}>
+            <div className={s.titleBlock}>
+                <h1 className={s.title}>Order History</h1>
+            </div>
 
         </div>
     );
--- src/features/profile/MainBlock/ProfileSettings/ProfileSettings.tsx
+++ src/features/profile/MainBlock/ProfileSettings/ProfileSettings.tsx
@@ -6,6 +6,24 @@ const ProfileSettings = () => {
 
     return (
         <div className={s.mainBlock}>
+            <div className={s.titleBlock}>
+                <h1 className={s.title}>Profile Settings</h1>
+            </div>
+            <div className={s.contentBlock}>
+                <div className={s.itemBlock}>
+                    <span className={s.itemTitle}>Full Name</span>
+                    <span className={s.itemDescription}>Nikita Znak</span>
+                    <span className={s.itemButton}>Edit</span>
+                </div>
+                <div className={s.itemBlock}>
+                    <span className={s.itemTitle}>Address</span>
+                    <span className={s.itemButton}>Add New</span>
+                </div>
+                <div className={s.itemBlock}>
+                    <span className={s.itemTitle}>Contact Phone Number</span>
+                    <span className={s.itemButton}>Add New</span>
+                </div>
+            </div>
 
         </div>
     );
