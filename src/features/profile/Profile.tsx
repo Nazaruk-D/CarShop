2e969f5ecca036bdf13f765e400d1bef03285158
naughtymor <znaknikita.it@gmail.com>
2023-02-28 22:47:24 +0300

    fix: fix route bug in Profile

--- src/features/profile/Profile.tsx
+++ src/features/profile/Profile.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import s from "./Profile.module.scss"
 import Header from "../../app/header/Header";
 import MainBlock from "./MainBlock/MainBlock";
+import {useAppSelector} from "../../app/store/store";
+import {routes} from "../../app/routes/routes";
+import {useNavigate} from "react-router-dom";
 
 const Profile = () => {
+    const isLoggedIn = useAppSelector(s => s.auth.isLoggedIn)
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if(!isLoggedIn) {
+            navigate(routes.login)
+        }
+    },[isLoggedIn])
 
     return (
         <div className={s.mainPage}>
