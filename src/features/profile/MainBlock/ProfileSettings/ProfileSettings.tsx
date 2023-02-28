014438569fad348fb23805f3851a2d60aee5538d
naughtymor <znaknikita.it@gmail.com>
2023-02-28 21:21:34 +0300

    refactor: ProfileSettings Component

--- src/features/profile/MainBlock/ProfileSettings/ProfileSettings.tsx
+++ src/features/profile/MainBlock/ProfileSettings/ProfileSettings.tsx
@@ -10,21 +10,37 @@ const ProfileSettings = () => {
                 <h1 className={s.title}>Profile Settings</h1>
             </div>
             <div className={s.contentBlock}>
-                <div className={s.itemBlock}>
-                    <span className={s.itemTitle}>Full Name</span>
-                    <span className={s.itemDescription}>Nikita Znak</span>
-                    <span className={s.itemButton}>Edit</span>
+                <div className={s.mainSettings}>
+                    <div className={s.itemBlock}>
+                        <span className={s.itemTitle}>Full Name</span>
+                        <span className={s.itemDescription}>Nikita Znak</span>
+                        <span className={s.itemButton}>Edit</span>
+                    </div>
+                    <div className={s.itemBlock}>
+                        <span className={s.itemTitle}>Address</span>
+                        <span className={s.itemButton}>Add New</span>
+                    </div>
+                    <div className={s.itemBlock}>
+                        <span className={s.itemTitle}>Contact Phone Number</span>
+                        <span className={s.itemButton}>Add New</span>
+                    </div>
                 </div>
-                <div className={s.itemBlock}>
-                    <span className={s.itemTitle}>Address</span>
-                    <span className={s.itemButton}>Add New</span>
+                <div className={s.securityTitleBlock}>
+                    <h1 className={s.securityTitle}>Security</h1>
                 </div>
-                <div className={s.itemBlock}>
-                    <span className={s.itemTitle}>Contact Phone Number</span>
-                    <span className={s.itemButton}>Add New</span>
+                <div className={s.securitySettings}>
+                    <div className={s.itemBlock}>
+                        <span className={s.itemTitle}>Email</span>
+                        <span className={s.itemDescription}>nikita.znak@mail.ru</span>
+                        <span className={s.itemButton}>Edit</span>
+                    </div>
+                    <div className={s.itemBlock}>
+                        <span className={s.itemTitle}>Password</span>
+                        <span className={s.itemDescription}>* * * * * * * * * * * *</span>
+                        <span className={s.itemButton}>Reset</span>
+                    </div>
                 </div>
             </div>
-
         </div>
     );
 };
