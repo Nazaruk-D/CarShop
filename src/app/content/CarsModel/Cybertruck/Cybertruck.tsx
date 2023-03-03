db774548742522dd19ee2b3ae9564a53dec07690
naughtymor <znaknikita.it@gmail.com>
2023-03-04 01:14:22 +0300

    refactor: video player to CybertruckFeatures Component

--- src/app/content/CarsModel/Cybertruck/Cybertruck.tsx
+++ src/app/content/CarsModel/Cybertruck/Cybertruck.tsx
@@ -4,6 +4,8 @@ import Header from "../../../header/Header";
 import Exoskeleton from "./Exoskeleton/Exoskeleton";
 import CybertruckFeatures from "../commonComponents/CybertruckFeatures/CybertruckFeatures";
 import cybertruckSkeleton from "../../../../common/assets/cybertruckSkeleton.jpg"
+//@ts-ignore
+import cybertruckVideo from "../../../../common/assets/video/cybertruckVideo.mp4"
 import cybertruckRoad from "../../../../common/assets/cybertruckRoad.jpg"
 import CybertruckCarousel from "./CybertruckCarousel/CybertruckCarousel";
 
@@ -18,6 +20,7 @@ const Cybertruck = () => {
             <CybertruckFeatures backgroundImage={cybertruckSkeleton} title={'ULTRA-HARD 30X COLD-ROLLED STAINLESS STEEL'} description={'If there was something better, we’d use it. Help eliminate dents, damage and long-term corrosion with a smooth monochrome exoskeleton that puts the shell on the outside of the car and provides you and your passengers maximum protection.'}/>
             <CybertruckFeatures backgroundImage={cybertruckRoad} title={'TESLA ARMOR GLASS'} description={'Ultra-strong glass and polymer-layered composite can absorb and redirect impact force for improved performance and damage tolerance.'}/>
             <CybertruckCarousel/>
+            <CybertruckFeatures titleSize={'big'} backgroundImage={cybertruckVideo} title={'PERFORMANCE AND EFFICIENCY'} description={'Now entering a new class of strength, speed and versatility—only possible with an all-electric design. The powerful drivetrain and low center of gravity provides extraordinary traction control and torque—enabling acceleration from 0-60 mph in as little as 2.9 seconds and up to 500 miles of range.'}/>
         </div>
     );
 };
--- src/app/content/CarsModel/commonComponents/CybertruckFeatures/CybertruckFeatures.tsx
+++ src/app/content/CarsModel/commonComponents/CybertruckFeatures/CybertruckFeatures.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import s from "./CybertruckFeatures.module.scss"
+import VideoPlayer from "../../../../../common/components/VideoPlayer/VideoPlayer";
 
 export type CybertruckFeaturesPropsType = {
     backgroundImage: string
     title: string
     description: string
+    titleSize?: 'big' | 'default'
 }
 
-const CybertruckFeatures: React.FC<CybertruckFeaturesPropsType> = ({backgroundImage, title, description}) => {
+const CybertruckFeatures: React.FC<CybertruckFeaturesPropsType> = ({backgroundImage, title, description, titleSize}) => {
 
     return (
         <div className={s.mainBlock} >
             <div className={s.imageContainer}>
-                <div className={s.image} style={{backgroundImage: `url(${backgroundImage})`}}></div>
+                <div className={s.image} style={{backgroundImage: `url(${backgroundImage})`}}>
+                    <VideoPlayer link={backgroundImage} style={{width: "100%", height: "100%", objectFit: "cover"}}/>
+                </div>
             </div>
             <div className={s.titleContainer}>
-                <h2 className={s.title}>{title}</h2>
+                <h2 className={titleSize === 'big' ? s.bigTitle : s.title}>{title}</h2>
                 <span className={s.description}>{description}</span>
             </div>
         </div>
