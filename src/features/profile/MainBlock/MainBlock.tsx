29f78b9ddf65df69ece10a08f4b6d351a1167380
naughtymor <znaknikita.it@gmail.com>
2023-03-05 19:16:11 +0300

    refactor: user and admin data

--- src/features/profile/MainBlock/MainBlock.tsx
+++ src/features/profile/MainBlock/MainBlock.tsx
@@ -1,12 +1,12 @@
 import React, {useState} from 'react';
 import s from "./MainBlock.module.scss"
-import {useAppDispatch} from "../../../app/store/store";
+import {useAppDispatch, useAppSelector} from "../../../app/store/store";
 import {useNavigate} from "react-router-dom";
 import {logoutTC} from "../../auth/auth-reducer";
 import {routes} from "../../../app/routes/routes";
 import ProfileSettings from "./ProfileSettings/ProfileSettings";
 import Orders from "./Orders/Orders";
-import { FiUser, FiArchive, FiLogOut } from "react-icons/fi";
+import { FiUser, FiArchive, FiLogOut, FiUsers } from "react-icons/fi";
 import {useModal} from "../Modal/useModal";
 import Modal from "../Modal/Modal";
 import {IconType} from "react-icons";
@@ -15,9 +15,15 @@ export type MainPropsType = {
     setModalActive: (modalActive: boolean) => void
 }
 
-const navData: NavDataType[] = [
+const userNavData: NavDataType[] = [
     {id: 0, name: 'Profile Settings', icon: <FiUser/>},
-    {id: 1, name: 'Orders', icon: <FiArchive/>},
+    {id: 1, name: 'Orders History', icon: <FiArchive/>},
+]
+
+const adminNavData: NavDataType[] = [
+    {id: 0, name: 'Profile Settings', icon: <FiUser/>},
+    {id: 1, name: 'Orders Panel', icon: <FiArchive/>},
+    {id: 2, name: 'Users List', icon: <FiUsers/>},
 ]
 
 type NavDataType = {
@@ -30,6 +36,7 @@ const MainBlock: React.FC<MainPropsType> = ({setModalActive}) => {
     const [item, setItem] = useState(1)
     const dispatch = useAppDispatch()
     const navigate = useNavigate()
+    const {role} = useAppSelector(s => s.profile.user)
 
     const onClickHandler = () => {
         dispatch(logoutTC())
@@ -40,10 +47,17 @@ const MainBlock: React.FC<MainPropsType> = ({setModalActive}) => {
         <div className={s.mainBlock}>
             <div className={s.navBlock}>
                 <div className={s.navContainer}>
-                    {navData.map((b, i) => <div className={s.buttonBlock} onClick={() => setItem(i)}>
-                        <div className={s.icon} style={{backgroundColor: item === i ? 'rgba(1,1,1,0.05)' : '',}}>{b.icon}</div>
-                        <span style={{color: item === i ? 'black' : '',}} className={s.navButton} >{b.name}</span>
-                    </div>)}
+                    {role === 'admin'
+                    ? adminNavData.map((b, i) => <div className={s.buttonBlock} onClick={() => setItem(i)}>
+                                <div className={s.icon} style={{backgroundColor: item === i ? 'rgba(1,1,1,0.05)' : '',}}>{b.icon}</div>
+                                <span style={{color: item === i ? 'black' : '',}} className={s.navButton} >{b.name}</span>
+                            </div>)
+                    : userNavData.map((b, i) => <div className={s.buttonBlock} onClick={() => setItem(i)}>
+                                <div className={s.icon} style={{backgroundColor: item === i ? 'rgba(1,1,1,0.05)' : '',}}>{b.icon}</div>
+                                <span style={{color: item === i ? 'black' : '',}} className={s.navButton} >{b.name}</span>
+                            </div>)
+                    }
+
                     <div className={s.buttonBlock}>
                         <div className={s.icon}><FiLogOut/></div>
                         <span onClick={onClickHandler} className={s.navButton}>Sign Out</span>
@@ -52,7 +66,9 @@ const MainBlock: React.FC<MainPropsType> = ({setModalActive}) => {
             </div>
             <div className={s.contentContainer}>
                 {item === 0 && <ProfileSettings setModalActive={setModalActive}/>}
-                {item === 1 && <Orders/>}
+                {item === 1 && role === 'user' && <Orders/>}
+                {item === 1 && role === 'admin' && <div>Orders Panel</div>}
+                {item === 2 && role === 'admin' && <div>Users List</div>}
             </div>
         </div>
     );
