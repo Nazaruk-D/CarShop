98e40cae1b864a836ab222c40028cd3df30f1d2b
Nazaruk-D <nazaruk-dima@mail.ru>
2023-03-07 23:49:02 +0300

    refactor: wheel scroling

--- src/app/content/MainPage/MainPage.tsx
+++ src/app/content/MainPage/MainPage.tsx
@@ -10,22 +10,36 @@ import SolarPanels from "./SolarPanels/SolarPanels";
 import SolarRoof from "./SolarRoof/SolarRoof";
 import Accessories from "./Accessories/Accessories";
 
+
 const cars = ['Model 3', 'Model Y', 'Model S', 'Model X', 'Solar Panels', 'Solar Roof', 'Accessories']
 
 const MainPage = () => {
     const [scroll, setScroll] = useState(0)
+    let shouldWait = false
 
     const handleClickScroll = (e: WheelEvent) => {
-        if (e.deltaY > 50) {
-            setScroll(prev => prev + 1)
-        } else if (e.deltaY < -50) {
-            setScroll(prev => prev - 1)
+        if (e.deltaY > 10) {
+            if (!shouldWait) {
+                setScroll((prev: number) => prev + 1)
+                shouldWait = true
+                setTimeout(function () {
+                    shouldWait = false
+                }, 200)
+            }
+        } else if (e.deltaY < -10) {
+            if (!shouldWait) {
+                setScroll((prev: number) => prev - 1)
+                shouldWait = true
+                setTimeout(function () {
+                    shouldWait = false
+                }, 200)
+            }
         }
     };
 
     useEffect(() => {
         window.addEventListener("wheel", (e) => handleClickScroll(e))
-        return () => window.removeEventListener("wheel", (e) => handleClickScroll(e));
+        return window.removeEventListener("wheel", (e) => handleClickScroll(e))
     }, []);
 
     useEffect(() => {
@@ -35,24 +49,30 @@ const MainPage = () => {
         if (scroll < 0) {
             setScroll(0)
         }
-        scroller.scrollTo(cars[scroll], {
-            duration: 200,
-            smooth: true,
-            offset: 0,
-        })
     }, [scroll, cars])
 
+
+    useEffect(() => {
+        setTimeout(function () {
+            scroller.scrollTo(cars[scroll], {
+                duration: 200,
+                smooth: true,
+                offset: 0,
+            })
+        }, 210)
+    }, [scroll])
+
     return (
-            <div className={s.contentContainer}>
-                <Header position={'fixed'}/>
-                <Model3/>
-                <ModelY/>
-                <ModelS/>
-                <ModelX/>
-                <SolarPanels/>
-                <SolarRoof/>
-                <Accessories/>
-            </div>
+        <div className={s.contentContainer}>
+            <Header position={'fixed'}/>
+            <Model3/>
+            <ModelY/>
+            <ModelS/>
+            <ModelX/>
+            <SolarPanels/>
+            <SolarRoof/>
+            <Accessories/>
+        </div>
     );
 };
 
---
+++ src/helpers/throttle.js
@@ -0,0 +1,14 @@
+export function throttle(func, duration) {
+    let shouldWait = false
+
+    return function (...args) {
+        if (!shouldWait) {
+            func.apply(this, args)
+            shouldWait = true
+
+            setTimeout(function () {
+                shouldWait = false
+            }, duration)
+        }
+    }
+}
