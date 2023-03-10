d33d49e154ccb6f923d4903887b0f16a2af0f034
Nazaruk-D <nazaruk-dima@mail.ru>
2023-03-10 11:59:09 +0300

    style: Characterisrics tsx

--- src/app/content/Ordering/Modification/Modification.tsx
+++ src/app/content/Ordering/Modification/Modification.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import s from "./Modification.module.scss"
-import Title from "./TItle/Title";
+import Title from "./Title/Title";
+import Characteristics from "../../../../common/components/Characteristics/Characteristics";
 
 const Modification = () => {
     return (
         <div className={s.modificationContainer}>
             <div className={s.modificationBlock}>
                 <Title/>
+                <div className={s.characteristicsBlock}>
+                    <Characteristics topText={"315"} measurement={"mi"} bottomText={"Range (EPA est.)"}/>
+                    <Characteristics topText={"162"} measurement={"mph"} bottomText={"Top speed"}/>
+                    <Characteristics topText={"3.1"} measurement={"sec"} bottomText={"0-60 mph"}/>
+                </div>
             </div>
         </div>
     );
