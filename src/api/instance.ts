f4ccbec4b25a78f20a357441f70973c534d6a6be
Nazaruk-D <nazaruk-dima@mail.ru>
2023-03-12 23:30:59 +0300

    refactor: new baseURL

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-    // baseURL: 'https://backend-tesla-dhyygdagn-nazaruk-d.vercel.app',
-    baseURL: 'http://localhost:7542/',
+    baseURL: 'https://backend-tesla-qwyt2za5g-nazaruk-d.vercel.app',
+    // baseURL: 'https://backend-tesla-dhyygdagn-nazaruk-d.vercel.app', // старая рабочая версия бэка
+    // baseURL: 'http://localhost:7542/',
     withCredentials: true,
 })
