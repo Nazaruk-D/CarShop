26518663920601c2151ab2966755a8b0689ae21a
naughtymor <znaknikita.it@gmail.com>
2023-03-17 20:37:23 +0300

    refactor: Schedule List

--- src/features/profile/MainBlock/SchedulePanel/SchedulePanel.tsx
+++ src/features/profile/MainBlock/SchedulePanel/SchedulePanel.tsx
@@ -1,79 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import s from "./SchedulePanel.module.scss"
-import {AiOutlineEdit} from "react-icons/ai";
+import {useAppDispatch, useAppSelector} from "../../../../app/store/store";
+import {getUsersTC} from "../UsersList/users-reducer";
 
 const users = [
     {
+        model: 'Model Y',
         email: 'nikita.znak@mail.ru',
         firstName: 'Nikita',
         lastName: 'Znak',
-        createdAt: '01.01.2023',
-        updatedAt: '01.01.2023'
-    },
-    {
-        email: 'nazaruk-dima@mail.ru',
-        firstName: 'Dima',
-        lastName: 'Nazaruk',
-        createdAt: '02.01.2023',
-        updatedAt: '02.01.2023'
-    },
-    {
-        email: 'eznak06@mail.ru',
-        firstName: 'Katya',
-        lastName: 'Nazaruk',
-        createdAt: '03.01.2023',
-        updatedAt: '03.01.2023'
-    },
-    {
-        email: 'nikita.znak@mail.ru',
-        firstName: 'Nikita',
-        lastName: 'Znak',
-        createdAt: '01.01.2023',
-        updatedAt: '01.01.2023'
-    },
-    {
-        email: 'nazaruk-dima@mail.ru',
-        firstName: 'Dima',
-        lastName: 'Nazaruk',
-        createdAt: '02.01.2023',
-        updatedAt: '02.01.2023'
-    },
-    {
-        email: 'eznak06@mail.ru',
-        firstName: 'Katya',
-        lastName: 'Nazaruk',
-        createdAt: '03.01.2023',
-        updatedAt: '03.01.2023'
-    },
-    {
-        email: 'nikita.znak@mail.ru',
-        firstName: 'Nikita',
-        lastName: 'Znak',
-        createdAt: '01.01.2023',
-        updatedAt: '01.01.2023'
-    },
-    {
-        email: 'nazaruk-dima@mail.ru',
-        firstName: 'Dima',
-        lastName: 'Nazaruk',
-        createdAt: '02.01.2023',
-        updatedAt: '02.01.2023'
+        region: 'BLR',
+        phoneNumber:'+375445316892',
+        status: 'pending',
+        contactPreference: 'Phone Number',
+        updatedAt: '01.01.2023',
     },
 ]
 
 
 const SchedulePanel = () => {
+    const {currentPage, pageSize, totalUsersCount} = useAppSelector(s => s.users)
+    const dispatch = useAppDispatch();
 
-
-    let pagesCount = Math.ceil(100 / 8);
+    let pagesCount = Math.ceil(totalUsersCount / pageSize);
     let pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
-    let currentPage = 5;
+
     let currentPageFirst = ((currentPage - 5) < 0 ? 0 : currentPage - 5);
     let currentPageLast = currentPage + 4;
     let slicedPages = pages.slice(currentPageFirst, currentPageLast);
+
+    useEffect(() => {
+        dispatch(getUsersTC({limit: pageSize, page: currentPage}))
+    },[totalUsersCount])
+
+    const onPageChange = (page: number) => {
+        dispatch(getUsersTC({limit: pageSize, page: page}))
+    }
 
     return (
         <div className={s.mainBlock}>
@@ -81,29 +46,40 @@ const SchedulePanel = () => {
                 <h1 className={s.title}>Schedule List</h1>
             </div>
             <div className={s.contentBlock}>
-                <div className={s.dataBlock}>
-                    {users.map((u, i) => (
-                        <div className={s.data} key={i}>
-                            <div className={s.dataFieldEmail}>{u.email}</div>
-                            <div className={s.dataFieldName}>{u.firstName} {u.lastName}</div>
-                            <div className={s.dataField}>Model Y</div>
-                            <div className={s.dataField}>{u.createdAt}</div>
-                            <div className={s.dataField}>{u.updatedAt}</div>
-                            <div className={s.dataFieldStatus}>
-                                <span className={s.status}>pending</span>
-                                <AiOutlineEdit/>
+                <div className={s.table}>
+                    <div className={s.tableTitles}>
+                        <div className={s.fieldSmall}>Model</div>
+                        <div className={s.fieldBig}>Email</div>
+                        <div className={s.fieldDefault}>Full Name</div>
+                        <div className={s.fieldSmall}>Region</div>
+                        <div className={s.fieldDefault}>Phone Number</div>
+                        <div className={s.fieldDefault}>Contact Preference</div>
+                        <div className={s.fieldDefault}>Updated At</div>
+                        <div className={s.fieldSmall}>Status</div>
+                    </div>
+                    <div className={s.tableData}>
+                        {users.map((u, i) => (
+                            <div className={s.data} key={i}>
+                                <div className={s.dataFieldRegion}>{u.model}</div>
+                                <div className={s.dataFieldEmail}>{u.email}</div>
+                                <div className={s.dataFieldHide}>{u.firstName} {u.lastName}</div>
+                                <div className={s.dataFieldRegion}>{u.region}</div>
+                                <div className={s.dataFieldHide}>{u.phoneNumber}</div>
+                                <div className={s.dataFieldHide}>{u.contactPreference}</div>
+                                <div className={s.dataFieldHide}>{u.updatedAt}</div>
+                                <div className={s.dataFieldRegion}>{u.status}</div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
                 <div className={s.paginationBlock}>
                     {slicedPages.map((p, index) => <span key={index}
                                                          className={currentPage === p ? s.selectedPage : s.page}
-                                                         onClick={() => {}}>{p + " "}</span>)}
+                                                         onClick={() => {onPageChange(p)}}>{p + " "}</span>)}
                 </div>
             </div>
         </div>
-    );
+    )
 };
 
 export default SchedulePanel;
