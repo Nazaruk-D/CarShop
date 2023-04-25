ae67e56f937df7bd88a51490cfe025143213ab19
naughtymor <znaknikita.it@gmail.com>
2023-04-25 17:48:34 +0300

    feat: json with demo-orders list

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-    // baseURL: 'https://backend-tesla-git-main-nazaruk-d.vercel.app',
+    baseURL: 'https://backend-tesla-nazaruk-d.vercel.app',
     // baseURL: 'https://backend-tesla-dhyygdagn-nazaruk-d.vercel.app', // старая рабочая версия бэка
-    baseURL: 'http://localhost:7542/',
+    // baseURL: 'http://localhost:7542/',
     withCredentials: true,
 })
