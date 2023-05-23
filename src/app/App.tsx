fb415a6172c0070b8125faad6bf9797493180e6c
Nazaruk-D <nazaruk-dima@mail.ru>
2023-05-23 19:20:01 +0300

    feat: UpArrow button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,12 +2,17 @@ import React, {useEffect} from 'react';
 import {useAppDispatch, useAppSelector} from "./store/store";
 import {initializeAppTC} from "./app-reducer";
 import {router} from "./routes/routes";
-import {RouterProvider} from "react-router-dom";
+import {RouterProvider, useLocation} from "react-router-dom";
 import Preloader from "../common/components/Preloader/Preloader";
+import UpArrow from "../common/components/UpArrow/UpArrow";
 
 const App = () => {
     const dispatch = useAppDispatch()
     const isInitialized = useAppSelector(s => s.app.initialized)
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    } );
 
     useEffect(() => {
         dispatch(initializeAppTC())
@@ -20,6 +25,7 @@ const App = () => {
     return (
         <>
             <RouterProvider router={router}/>
+            <UpArrow/>
         </>
     );
 };
